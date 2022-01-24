fca967e9c12dc0ef4a17d2b33910acc6b564fc29
James-Durant <43757679+James-Durant@users.noreply.github.com>
2022-01-24 13:39:50 +0000

    Updated GUI

--- hexcells-solver/main.py
+++ hexcells-solver/main.py
@@ -1,4 +1,6 @@
+import os
 import tkinter as tk
+from tkinter import filedialog
 
 from subprocess import Popen
 from navigate import Navigator
@@ -7,17 +9,19 @@ GAMEIDS = {'Hexcells': '265890',
            'Hexcells Plus': '271900',
            'Hexcells Infinite': '304410'}
 
+
 class GUI:
     def __init__(self):
         self.__root = tk.Tk()
         self.__root.title('HexSolver')
         self.__root.resizable(width=False, height=False)
-        #self.__window_width = 300
-        #self.__window_height = 600
-        #self.__root.geometry('{0}x{1}+0+0'.format(self.__window_width, self.__window_height))
+        # self.__window_width = 300
+        # self.__window_height = 600
+        # self.__root.geometry('{0}x{1}+0+0'.format(self.__window_width, self.__window_height))
 
         self.__create_info_frame()
         self.__create_solver_frame()
+        self.__create_learning_frame()
 
         self.__check_game_running()
         tk.mainloop()
@@ -28,7 +32,7 @@ class GUI:
             self.__update_status(True)
             self.__game_var.set(self.__menu.title)
 
-        except RuntimeError: # Make custom error
+        except RuntimeError:  # Make custom error
             self.__update_status(False)
             self.__menu = None
             self.__game_var.set(-1)
@@ -52,7 +56,7 @@ class GUI:
 
         self.__root.update()
         Popen([r'C:\Program Files (x86)\Steam\steam.exe',
-               r'steam://rungameid/'+GAMEIDS[self.__game_var.get()]],
+               r'steam://rungameid/' + GAMEIDS[self.__game_var.get()]],
               shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
 
         while True:
@@ -78,7 +82,7 @@ class GUI:
 
         self.__info_label = tk.Label(self.__info_frame,
                                      font=('Arial Black', 10),
-                                     text='-'*70+'\n'+'Game Options\n'+'-'*70)
+                                     text='-' * 70 + '\n' + 'Game Options\n' + '-' * 70)
 
         self.__game_var = tk.StringVar(self.__root)
         self.__game_var.set(-1)
@@ -112,8 +116,8 @@ class GUI:
 
         self.__save_frame = tk.Frame(self.__info_frame)
         self.__save_label = tk.Label(self.__save_frame,
-                                    font=('Arial', 9),
-                                    text='Select Save:')
+                                     font=('Arial', 9),
+                                     text='Select Save:')
 
         self.__save_optionmenu = tk.OptionMenu(self.__save_frame,
                                                self.__save_var,
@@ -139,7 +143,7 @@ class GUI:
 
         self.__solve_label = tk.Label(self.__solver_frame,
                                       font=('Arial Black', 10),
-                                      text='-'*70+'\n'+'Solver Options\n'+'-'*70)
+                                      text='-' * 70 + '\n' + 'Solver Options\n' + '-' * 70)
 
         self.__level_radiobutton = tk.Radiobutton(self.__solver_frame,
                                                   variable=self.__solve_var,
@@ -255,21 +259,180 @@ class GUI:
 
     def __check_ready_to_solve(self, *args):
         if ((not self.__game_running) or
-            (self.__solve_var.get() == 0 and (self.__set_var.get() == '-' or self.__level_var.get() == '-')) or
-            (self.__solve_var.get() == 1 and self.__set_var.get() == '-')):
+                (self.__solve_var.get() == 0 and (self.__set_var.get() == '-' or self.__level_var.get() == '-')) or
+                (self.__solve_var.get() == 1 and self.__set_var.get() == '-')):
             self.__solve_button.configure(state=tk.DISABLED)
         else:
             self.__solve_button.configure(state=tk.NORMAL)
 
     def __solve(self):
         if self.__solve_var.get() == 0:
-            self.__menu.solve_level(self.__save_var.get(), self.__set_var.get()+'-'+self.__level_var.get())
+            self.__menu.solve_level(self.__save_var.get(), self.__set_var.get() + '-' + self.__level_var.get())
         elif self.__solve_var.get() == 1:
             self.__menu.solve_set(self.__save_var.get(), self.__set_var.get())
         elif self.__solve_var.get() == 2:
             self.__menu.solve_game(self.__save_var.get())
         elif self.__solve_var.get() == 3:
             self.__menu.level_generator()
+
+    def __create_learning_frame(self):
+        self.__learning_frame = tk.Frame(self.__root)
+
+        self.__model_var = tk.IntVar(self.__root)
+        self.__model_var.set(0)
+
+        self.__learning_label = tk.Label(self.__learning_frame,
+                                         font=('Arial Black', 10),
+                                         text='-' * 70 + '\n' + 'Learning Options\n' + '-' * 70)
+
+        self.__new_radiobutton = tk.Radiobutton(self.__learning_frame,
+                                                variable=self.__model_var,
+                                                value=0,
+                                                text='New Model',
+                                                font=('Arial Black', 9))
+
+        self.__load_radiobutton = tk.Radiobutton(self.__learning_frame,
+                                                 variable=self.__model_var,
+                                                 value=1,
+                                                 text='Load Model',
+                                                 font=('Arial Black', 9))
+
+        modes = ['Online', 'Offline']
+        self.__mode_var = tk.StringVar(self.__root)
+        self.__mode_var.set('Online')
+
+        self.__mode_frame = tk.Frame(self.__learning_frame)
+        self.__mode_label = tk.Label(self.__mode_frame,
+                                     font=('Arial', 9),
+                                     text='Select Mode:')
+
+        self.__mode_optionmenu = tk.OptionMenu(self.__mode_frame,
+                                               self.__mode_var,
+                                               *modes)
+        self.__mode_label.pack(side='left')
+        self.__mode_optionmenu.pack()
+
+        self.__test_var = tk.IntVar(self.__root)
+        self.__test_var.set(0)
+
+        self.__test_checkbutton = tk.Checkbutton(self.__learning_frame,
+                                                 text='Test Only?',
+                                                 variable=self.__test_var,
+                                                 onvalue=1,
+                                                 offvalue=0)
+
+        self.__mode_frame.grid(sticky='w', row=1, column=1)
+        self.__test_checkbutton.grid(sticky='w', row=2, column=1)
+
+        self.__learning_label.grid(sticky='w', row=0, column=0, columnspan=2)
+        self.__new_radiobutton.grid(sticky='w', row=1, column=0)
+        self.__load_radiobutton.grid(sticky='w', row=2, column=0)
+
+        self.__epochs_frame = tk.Frame(self.__learning_frame)
+        self.__epochs_label = tk.Label(self.__epochs_frame,
+                                       text='Epochs: ')
+
+        self.__epochs_var = tk.StringVar()
+        self.__epochs_var.set('10')
+        self.__epochs_entry = tk.Entry(self.__epochs_frame,
+                                       textvariable=self.__epochs_var)
+
+        self.__epochs_label.pack(side='left')
+        self.__epochs_entry.pack(expand=True, fill='both')
+        self.__epochs_frame.grid(sticky='nesw', row=4, column=0, columnspan=2)
+
+        self.__batch_size_frame = tk.Frame(self.__learning_frame)
+        self.__batch_size_label = tk.Label(self.__batch_size_frame,
+                                           text='Batch Size: ')
+
+        self.__batch_size_var = tk.StringVar()
+        self.__batch_size_var.set('64')
+        self.__batch_size_entry = tk.Entry(self.__batch_size_frame,
+                                           textvariable=self.__batch_size_var)
+
+        self.__batch_size_label.pack(side='left')
+        self.__batch_size_entry.pack(expand=True, fill='both')
+        self.__batch_size_frame.grid(sticky='nesw', row=5, column=0, columnspan=2)
+
+        self.__learning_rate_frame = tk.Frame(self.__learning_frame)
+        self.__learning_rate_label = tk.Label(self.__learning_rate_frame,
+                                              text='Learning Rate: ')
+
+        self.__learning_rate_var = tk.StringVar()
+        self.__learning_rate_var.set('0.01')
+        self.__learning_rate_entry = tk.Entry(self.__learning_rate_frame,
+                                              textvariable=self.__learning_rate_var)
+
+        self.__learning_rate_label.pack(side='left')
+        self.__learning_rate_entry.pack(expand=True, fill='both')
+        self.__learning_rate_frame.grid(sticky='nesw', row=6, column=0, columnspan=2)
+
+        self.__discount_frame = tk.Frame(self.__learning_frame)
+        self.__discount_label = tk.Label(self.__discount_frame,
+                                         text='Discount Rate: ')
+
+        self.__discount_var = tk.StringVar()
+        self.__discount_var.set('0.05')
+        self.__discount_entry = tk.Entry(self.__discount_frame,
+                                         textvariable=self.__discount_var)
+
+        self.__discount_label.pack(side='left')
+        self.__discount_entry.pack(expand=True, fill='both')
+        self.__discount_frame.grid(sticky='nesw', row=7, column=0, columnspan=2)
+
+        self.__exploration_rate_frame = tk.Frame(self.__learning_frame)
+        self.__exploration_rate_label = tk.Label(self.__exploration_rate_frame,
+                                                 text='Exploration Rate: ')
+
+        self.__exploration_rate_var = tk.StringVar()
+        self.__exploration_rate_var.set('0.95')
+        self.__exploration_rate_entry = tk.Entry(self.__exploration_rate_frame,
+                                                 textvariable=self.__exploration_rate_var)
+
+        self.__exploration_rate_label.pack(side='left')
+        self.__exploration_rate_entry.pack(expand=True, fill='both')
+        self.__exploration_rate_frame.grid(sticky='nesw', row=8, column=0, columnspan=2)
+
+        self.__path_frame = tk.Frame(self.__learning_frame)
+        self.__path_button = tk.Button(self.__path_frame,
+                                       text='Select Model: ',
+                                       command=self.__select_model_path)
+
+        self.__model_path_var = tk.StringVar()
+        self.__model_path_var.set('')
+        self.__path_entry = tk.Entry(self.__path_frame,
+                                     textvariable=self.__model_path_var,
+                                     state=tk.DISABLED)
+
+        self.__path_button.pack(side='left')
+        self.__path_entry.pack(expand=True, fill='both')
+        self.__path_frame.grid(sticky='nesw', row=9, column=0, columnspan=2, pady=10)
+
+        self.__train_button = tk.Button(self.__learning_frame,
+                                        text='Run',
+                                        font=('Arial', 10),
+                                        command=self.__train)
+
+        self.__train_button.grid(sticky='nesw', row=10, column=0, columnspan=2, pady=(10, 0))
+
+        self.__learning_frame.grid(row=2, column=0)
+
+    def __train(self):
+        if self.__mode_var.get() == 'Offline':
+            pass
+
+        elif self.__mode_var.get() == 'Online':
+            pass
+
+    def __select_model_path(self, *args):
+        file_path = os.path.dirname(os.path.realpath(__file__))
+        models_dir = os.path.join(file_path, 'resources', 'models')
+        model_path = filedialog.askopenfilename(parent=self.__root,
+                                                initialdir=models_dir,
+                                                title='Model Selection',
+                                                filetypes=[('HDF5 file', '.h5')])
+        self.__model_path_var.set(model_path)
+
 
 if __name__ == '__main__':
     application = GUI()
