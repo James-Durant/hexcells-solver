416234cbc0a902c61770f9f249d41cd8d037c30e
James-Durant <43757679+James-Durant@users.noreply.github.com>
2022-02-21 09:12:56 +0000

    Added models and figures

--- hexcells-solver/plot.py
+++ hexcells-solver/plot.py
@@ -5,17 +5,19 @@ import matplotlib.pyplot as plt
 COLOURS = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 
-def plot_accuracy(log_path, models, file_name):
+def plot_accuracy(log_path, models, file_name, labels=None):
     fig = plt.figure(figsize=[9, 7], dpi=600)
     ax = fig.add_subplot(111)
 
-    for model, colour in zip(models, COLOURS):
+    for i, (model, colour) in enumerate(zip(models, COLOURS)):
         accuracy_history = np.loadtxt(os.path.join(log_path, f'model_{model}.csv'), delimiter=',')
         level_nums = accuracy_history[:, 0]
         train_accuracy = accuracy_history[:, 1]
         test_accuracy = accuracy_history[:, 2]
 
-        ax.plot(level_nums, train_accuracy, color=colour, linestyle='solid', linewidth=0.8, label=f'Model {model}')
+        label = f'Model {model}' if labels is None else labels[i]
+
+        ax.plot(level_nums, train_accuracy, color=colour, linestyle='solid', linewidth=0.8, label=label)
         ax.plot(level_nums, test_accuracy, color=colour, linestyle='dashed', linewidth=0.8)
 
     ax.set_xlabel('Number of Levels Solved', fontsize=11, weight='bold')
@@ -31,6 +33,6 @@ def plot_accuracy(log_path, models, file_name):
 
 if __name__ == '__main__':
     plot_accuracy('resources/models/logs', [1, 2, 3, 4], 'comparison_nodes_layers')
-    # plot_accuracy('resources/models/logs', [5, 6, 7, 8], 'comparison_activation_filters')
-    # plot_accuracy('resources/models/logs', [9, 10, 11], 'comparison_replay_double')
-    # plot_accuracy('resources/models/logs', [12, 13, 14], 'comparison_level_sizes')
+    plot_accuracy('resources/models/logs', [3, 5, 6, 7], 'comparison_activation_filters')
+    plot_accuracy('resources/models/logs', [3, 8, 9], 'comparison_replay_double')
+    plot_accuracy('resources/models/logs', [3, 10, 11], 'comparison_level_sizes', labels=['Small', 'Medium', 'Large'])
