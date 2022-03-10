6f3fad9690e41bf04861e3034e6b451153c3d27c
James-Durant <43757679+James-Durant@users.noreply.github.com>
2022-03-10 11:52:08 +0000

    Commented plot.py

--- hexcells-solver/plot.py
+++ hexcells-solver/plot.py
@@ -2,76 +2,79 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-COLOURS = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
+def plot_accuracy(log_path, models, file_name, split, labels=None):
+    """Plot training and validation accuracy versus number of levels solved.
 
-def plot_accuracy_single(log_path, models, file_name, labels=None):
-    fig = plt.figure(figsize=[9, 7], dpi=600)
-    ax = fig.add_subplot(111)
-
-    for i, (model, colour) in enumerate(zip(models, COLOURS)):
-        accuracy_history = np.loadtxt(os.path.join(log_path, f'model_{model}.csv'), delimiter=',')
-        level_nums = accuracy_history[:, 0]
-        train_accuracy = accuracy_history[:, 1]
-        val_accuracy = accuracy_history[:, 2]
-
-        label = f'Model {model}' if labels is None else labels[i]
-
-        ax.plot(level_nums, train_accuracy, color=colour, label=label, linewidth=0.8, linestyle='solid')
-        ax.plot(level_nums, val_accuracy, color=colour, linewidth=0.8, linestyle='dashed')
-
-    ax.set_xlabel('Number of Levels Solved', fontsize=11, weight='bold')
-    ax.set_ylabel('Accuracy', fontsize=11, weight='bold')
-    ax.set_ylim(0.35, 1.05)
-    ax.legend()
-
-    save_path = os.path.join(log_path, '..', 'figures')
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-
-    fig.savefig(os.path.join(save_path, f'{file_name}.png'), dpi=300)
-
-
-def plot_accuracy_split(log_path, models, file_name, labels=None):
+    Args:
+        log_path (str): file path to the log containing accuracies to plot.
+        models (list): models to load logs for.
+        file_name (str): file name to use when saving the plot.
+        split (bool): whether to use separate plots for training and validation accuracies.
+        labels (list, optional): labels to use for the plot legend(s).
+    """
+    # Create the figures.
     fig1 = plt.figure(figsize=[9, 7], dpi=600)
-    ax1 = fig1.add_subplot(111)
-
     fig2 = plt.figure(figsize=[9, 7], dpi=600)
+    ax1 = fig1.add_subplot(111)
     ax2 = fig2.add_subplot(111)
 
-    for i, (model, colour) in enumerate(zip(models, COLOURS)):
+    # The default matplotlib colour cycle.
+    colours = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+    # Iterate over each model.
+    for i, (model, colour) in enumerate(zip(models, colours)):
+        # Load the log for the model.
         accuracy_history = np.loadtxt(os.path.join(log_path, f'model_{model}.csv'), delimiter=',')
+
+        # Get the training and validation accuracies and the number of levels solved at each step.
         level_nums = accuracy_history[:, 0]
         train_accuracy = accuracy_history[:, 1]
         val_accuracy = accuracy_history[:, 2]
 
+        # Label for the legend.
         label = f'Model {model}' if labels is None else labels[i]
 
+        # Plot the training and validation accuracies.
         ax1.plot(level_nums, train_accuracy, color=colour, label=label, linewidth=0.8, linestyle='solid')
-        ax2.plot(level_nums, val_accuracy, color=colour, label=label, linewidth=0.8, linestyle='dashed')
+        ax1.plot(level_nums, val_accuracy, color=colour, linewidth=0.8, linestyle='dashed')
+        ax2.plot(level_nums, val_accuracy, color=colour, linewidth=0.8, linestyle='dashed')
 
+    # Set the x-axis labels.
     ax1.set_xlabel('Number of Levels Solved', fontsize=11, weight='bold')
     ax2.set_xlabel('Number of Levels Solved', fontsize=11, weight='bold')
 
-    ax1.set_ylabel('Training Accuracy', fontsize=11, weight='bold')
-    ax2.set_ylabel('Validation Accuracy', fontsize=11, weight='bold')
+    # Set the y-axis labels.
+    if split:
+        ax1.set_ylabel('Training Accuracy', fontsize=11, weight='bold')
+        ax2.set_ylabel('Validation Accuracy', fontsize=11, weight='bold')
+    else:
+        ax1.set_ylabel('Accuracy', fontsize=11, weight='bold')
 
+    # Set the range for the y-axis.
     ax1.set_ylim(0.35, 1.05)
     ax2.set_ylim(0.35, 1.05)
 
+    # Add legends to the plots.
     ax1.legend()
     ax2.legend()
 
+    # Create a new directory for figures if one does not exist.
     save_path = os.path.join(log_path, '..', 'figures')
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
-    fig1.savefig(os.path.join(save_path, f'{file_name}_training.png'), dpi=300)
-    fig2.savefig(os.path.join(save_path, f'{file_name}_validation.png'), dpi=300)
+    # Save the figure(s).
+    if split:
+        fig1.savefig(os.path.join(save_path, f'{file_name}_training.png'), dpi=300)
+        fig2.savefig(os.path.join(save_path, f'{file_name}_validation.png'), dpi=300)
+    else:
+        fig1.savefig(os.path.join(save_path, f'{file_name}.png'), dpi=300)
 
 
 if __name__ == '__main__':
-    plot_accuracy_split('resources/models/logs', [1, 2, 3, 4, 5], 'comparison_nodes_layers')
-    plot_accuracy_split('resources/models/logs', [4, 6, 7, 8, 9], 'comparison_activation_filters')
-    plot_accuracy_single('resources/models/logs', [4, 10, 11], 'comparison_replay_double', labels=['Standard', 'Experience Replay', 'Double DQN'])
-    plot_accuracy_single('resources/models/logs', [4, 12, 13], 'comparison_level_sizes', labels=['Small', 'Medium', 'Large'])
+    # If this file is being run, create the plots used in the final report.
+    plot_accuracy('resources/models/logs', [1, 2, 3, 4, 5], 'comparison_nodes_layers', True)
+    plot_accuracy('resources/models/logs', [4, 6, 7, 8, 9], 'comparison_activation_filters', True)
+    plot_accuracy('resources/models/logs', [4, 10, 11], 'comparison_replay_double', False, ['Standard', 'Experience Replay', 'Double DQN'])
+    plot_accuracy('resources/models/logs', [4, 12, 13], 'comparison_level_sizes', False, ['Small', 'Medium', 'Large'])
